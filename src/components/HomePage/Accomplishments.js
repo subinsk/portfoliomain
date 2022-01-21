8a110e58c0ab5edd634899a7bfbf677a324e2f64
Subin <subinsk284@gmail.com>
2022-01-21 15:21:34 +0530

    accomplishments done

--- src/components/HomePage/Accomplishments.js
+++ src/components/HomePage/Accomplishments.js
@@ -1,25 +1,57 @@
 import React from 'react';
 import Image from 'next/image'
 
+import Tooltip from '../../hooks/tooltip';
+
 import HackJNU from '../../images/certificates/hackjnu.jpg'
+import Cicada from '../../images/certificates/cicada_certificate.jpg'
+import Octahacks from '../../images/certificates/octahacks.png'
 
 function Accomplishments() {
     return <div className='flex flex-col w-full'>
         <p className='flex bg-gradient-to-r from-custom-purple-300 to-custom-cyan bg-clip-text text-transparent text-5xl p-12 font-medium after:content-[""] after:relative after:inline-block after:w-2/5 after:h-[1px] after:bg-custom-purple-50 after:mt-7 after:ml-4'>My Accomplishments till now</p>
-        <div className='flex flex-col'>
-            <div className='flex justify-evenly'>
-                <a href="https://www.dropbox.com/s/by789h6jrejo3is/HackJNU%202.0%20%3A%20Subin%20S%20K%20Cerificate.pdf?dl=0" target="_blank" rel="noopener noreferrer">
-                    <div className="flex w-[22rem] h-[17rem] overflow-hidden duration-700 rounded-lg shadow-xl hover:shadow-2xl">
-                        <Image src={HackJNU} alt='HackJNU' objectFit='cover' />
-                    </div>
-                </a>
-                <div className='flex flex-col items-end my-2'>
-                    <p className='text-base text-purple-700'>WINNER</p>
-                    <p className='text-4xl font-medium bg-gradient-to-r from-blue-800 to-custom-cyan bg-clip-text text-transparent'>HackJNU 2.0</p>
+        <div className='flex flex-col items-center'>
+            <div className='flex justify-evenly border-0 rounded-md shadow-lg space-x-20 bg-blue-100/30 max-w-[48.2rem]'>
+                <Tooltip tooltipText="Click to view Certificate">
+                    <a href="https://www.dropbox.com/s/by789h6jrejo3is/HackJNU%202.0%20%3A%20Subin%20S%20K%20Cerificate.pdf?dl=0" target="_blank" rel="noopener noreferrer">
+                        <div className="flex w-[22rem] h-[17rem] overflow-hidden duration-700 rounded-lg shadow-xl hover:shadow-2xl saturate-0 hover:saturate-150">
+                            <Image src={HackJNU} alt='HackJNU' objectFit='cover' />
+                        </div>
+                    </a>
+                </Tooltip>
+                <div className='flex flex-col items-end my-2 p-6'>
+                    <p className='text-xl font-medium bg-gradient-to-r from-purple-700 to-blue-700 bg-clip-text text-transparent'>WINNER</p>
+                    <p className='text-[3rem] font-medium bg-gradient-to-r from-blue-800 to-custom-cyan bg-clip-text text-transparent'>HackJNU 2.0</p>
+                </div>
+            </div>
+            <div className='flex justify-evenly border-0 rounded-md shadow-lg space-x-15 bg-blue-100/30 my-32 max-w-[48.2rem]'>
+                <div className='flex flex-col items-start my-2 p-6'>
+                    <p className='text-xl font-medium bg-gradient-to-r from-purple-700 to-blue-700 bg-clip-text text-transparent'>RUNNER-UP</p>
+                    <p className='text-[3rem] font-medium bg-gradient-to-r from-blue-800 to-custom-cyan bg-clip-text text-transparent'>Cicada 3301:Reinvented</p>
+                </div>
+                <Tooltip tooltipText="Click to view Certificate">
+                    <a href="https://www.dropbox.com/s/fwtr00ash2wugw5/Subin%20SK%20Certificate%20-%20Cicada%203301.pdf?dl=0" target="_blank" rel="noopener noreferrer">
+                        <div className="flex w-[22rem] h-[17rem] overflow-hidden duration-700 rounded-lg shadow-xl hover:shadow-2xl saturate-0 hover:saturate-150">
+                            <Image src={Cicada} alt='Cicada' objectFit='cover' />
+                        </div>
+                    </a>
+                </Tooltip>
+            </div>
+            <div className='flex justify-evenly border-0 rounded-md shadow-lg space-x-20 bg-blue-100/30 max-w-[48.2rem]'>
+                <Tooltip tooltipText="Click to view Certificate">
+                    <a href="https://www.dropbox.com/s/d1xlgzeqzurx91j/Octahacks2.0.pdf?dl=0" target="_blank" rel="noopener noreferrer">
+                        <div className="flex w-[22rem] h-[17rem] overflow-hidden duration-700 rounded-lg shadow-xl hover:shadow-2xl saturate-0 hover:saturate-150">
+                            <Image src={Octahacks} alt='Octahacks' objectFit='cover' />
+                        </div>
+                    </a>
+                </Tooltip>
+                <div className='flex flex-col items-end my-2 p-6'>
+                    <p className='text-xl font-medium bg-gradient-to-r from-purple-700 to-blue-700 bg-clip-text text-transparent'>RUNNER UP</p>
+                    <p className='text-[2.8rem] font-medium bg-gradient-to-r from-blue-800 to-custom-cyan bg-clip-text text-transparent'>Octahacks 2.0</p>
                 </div>
             </div>
         </div>
-    </div>;
+    </div>
 }
 
 export default Accomplishments;
