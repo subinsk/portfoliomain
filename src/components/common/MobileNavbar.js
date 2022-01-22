df8964d1ab0c91434bc1693dca8c02e66859e53d
Subin <subinsk284@gmail.com>
2022-01-22 22:48:50 +0530

    done

--- src/components/common/MobileNavbar.js
+++ src/components/common/MobileNavbar.js
@@ -6,7 +6,7 @@ function MobileNavbar() {
     const mobNavbarHandler = () => {
         setMobNavbarHandler(true)
     }
-    return <div className={`${showMobNavbar ? 'translate-y-full' : 'translate-y-0'} transition-all ease-in-out absolute top-0 w-full h-screen flex flex-col`}>
+    return <div className={`${showMobNavbar ? 'translate-y-full' : 'translate-y-0'} transition-all  ease-in-out absolute top-0 w-full h-screen flex flex-col`}>
         <svg onClick={() => mobNavbarHandler()} xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 absolute top-4 right-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
         </svg>
