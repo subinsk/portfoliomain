1c00e994d6a1b8ad70b4ae1d29eb2c0f2f695574
Subin S K <subinsk284@gmail.com>
2022-03-20 23:05:35 +0530

    footer updated with realtime year fetch

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 function Footer() {
+    const date = new Date().getFullYear();
     return (
         <div id="footer" className='z-50 bg-gradient-to-r from-custom-purple-400 to-custom-cyan text-white flex justify-center items-center h-24 w-full'>
-            Copyright &copy; 2021
+            Copyright &copy; {date}
             <br />
         </div>
     )
