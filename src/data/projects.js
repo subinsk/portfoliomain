21ed29d1a4f7a30598f0d7bc5457c46209025e1d
Subin S K <subinsk284@gmail.com>
2023-01-04 18:14:41 +0530

    modularised project and accomplishments section

---
+++ src/data/projects.js
@@ -0,0 +1,96 @@
+export const projects = [
+    {
+        name: "Furnerio",
+        description: "E-Commerce store, integrated with Product Information Management admin panel to manage the whole e-commerce infrastructure.",
+        techStack: [
+            "NextJS",
+            "NodeJS",
+            "TailwindCSS",
+            "Ant Design",
+            "Firebase",
+            "MongoDB",
+            "Vercel"
+        ],
+        github: "",
+        website: "https://www.furnerio.com/",
+        image: require('../images/projects/furnerio.png')
+    },
+    {
+        name: "Lostrace",
+        description: "Reverse Image Search based Tracking App with efficient Flex-Search Algorithm on WCD Lost-Child Data.",
+        techStack: [
+            "ReactJS",
+            "Flask",
+            "Heroku",
+            "Vercel"
+        ],
+        github: "https://github.com/ML9-FORCES/Lostrace-Frontend",
+        website: "https://lostrace.vercel.app/",
+        image: require('../images/projects/lostrace.png')
+    },
+    {
+        name: "Classroom-X",
+        description: "A Robust E-Classroom Platform With Lowest Data-Transmission Leveraging on AI for Overcoming the Internet Access Inequity Our Approach Was to Take Shortest Path Instead of Fastest Vehicle , So Instead of Direct Streaming We Regenerated Teacher&apos;s Screen on Student Console With Bare-Minimum Data-Streaming and Client-Side AI Monitoring Students Presence.",
+        techStack: [
+            "Tensorflow | BlazeFace",
+            "Flask",
+            "Python",
+            "MongoDB",
+            "Socket.IO",
+            "HTML/CSS/JS"
+        ],
+        github: "https://github.com/Garvit9000c/ML9-FORCES_CLASSROOM-X",
+        website: "https://classroom-x.herokuapp.com/",
+        image: require('../images/projects/classroom.jpg')
+    },
+    {
+        name: "Matrix.io",
+        description: " Landing Page for Matrix.io Student Community",
+        techStack: [
+            "ReactJS",
+            "SupaBase"
+        ],
+        github: "https://github.com/subinsk/matrixio",
+        website: "https://www.matrixio.tech/",
+        image: require('../images/projects/matrixio.png')
+    },
+    {
+        name: "Posmate",
+        description: "A Robust AI-Driven System (Web App) That Monitors : Posture (7 Body KeyPoints) Relative Distance from Display Which Notifies User when his/her Posture or Relative Distance is Wrong with a Suggestion on how to improve it!",
+        techStack: [
+            "MoveNet 3.0",
+            "Flask",
+            "HTML/CSS/JS"
+        ],
+        github: "https://github.com/subinsk/Posmate",
+        website: "https://posmate-io.herokuapp.com/",
+        image: require('../images/projects/posmate.jpg')
+    },
+    {
+        name: "GeekBot",
+        description: "Advance Discord Bot with all the Administrative Moderation Power, Codeforces and GitHub access, which can take your Tech Server to another level.",
+        techStack: [
+            "NodeJS",
+            "MongoDB",
+            "ExpressJS",
+            "DiscordJS"
+        ],
+        github: "https://github.com/subinsk/geekbot",
+        website: "",
+        image: require('../images/projects/geekbot.jpg')
+    },
+    {
+        name: "Extractify",
+        description: "Extractify is a Multi-Class Bert-Classifier & ANN Linker Based JSON data extractor, Extractify API Takes Raw-Ocr Data & Classify & Maps Key-Value pairs which is Extremely useful in data extraction.",
+        techStack: [
+            "ReactJS",
+            "Canvas",
+            "Flask",
+            "BERT Classifier Model",
+            "Tensorflow"
+        ],
+        github: "https://github.com/ML9-FORCES/extractify-frontend",
+        website: "https://extractifynow.netlify.app/",
+        image: require('../images/projects/extractify.png')
+    },
+]
