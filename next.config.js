9f0ef9144d48497b9b409d3be2adab4dc2db8627
Subin S K <subinsk284@gmail.com>
2023-10-23 07:05:16 +0530

    loader done

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 module.exports = {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.pdf$/i,
+      type: "asset/source",
+    });
+
+    return config;
+  },
   reactStrictMode: true,
   images: {
-    domains: [
-      "res.cloudinary.com",
-      "images.credential.net"
-    ]
-  }
-}
+    domains: ["res.cloudinary.com", "images.credential.net"],
+  },
+};
